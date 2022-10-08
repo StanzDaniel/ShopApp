966147809f654aa543a626054ce0e12e4346d7a5
Daniel Stanziola <stanzioladaniel@gmail.com>
2022-10-08 18:30:53 -0300

    Añadiendo Logo en Navbar

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+class Logo extends Component {
+  render() {
+    return (
+      <div>
+        <img alt='DanielShop' src='./logo.png' />
+      </div>
+    )
+  }
+}
+
+export default Logo
